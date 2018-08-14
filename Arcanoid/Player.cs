facdb659c184b47509e37dcf6efa0fc46fbe46f6
D34DStone <deadstonepro@gmail.com>
2018-08-14 15:07:23 +0300

    another one

--- Arcanoid/Player.cs
+++ Arcanoid/Player.cs
@@ -10,11 +10,11 @@ namespace Arcanoid.Desktop
 		private const float SPEED = 0.25F;
 
 		public Player(Rectangle mask) : base(mask) { }
-  
+
 		public void Update(KeyboardState keyboardState, GameTime gameTime, Vector2 windowSize)
 		{
 			float msElapsed = gameTime.ElapsedGameTime.Milliseconds;
-   
+
 			if (keyboardState.IsKeyDown(Keys.A) && mask.Left >= 0)
 			{
 				mask.Offset(new Vector2(-SPEED * msElapsed, 0));
---
+++ Arcanoid/Solid.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Arcanoid.Desktop
+{
+    abstract class Solid
+    {
+        protected Texture2D texture;
+        protected Rectangle mask;
+
+        public Solid(Rectangle mask)
+        {
+            this.mask = mask;
+        }
+
+        public void LoadContent(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, mask, Color.White);
+        }
+
+        public Rectangle Mask
+        {
+            get { return mask; }
+        }
+    }
+}
